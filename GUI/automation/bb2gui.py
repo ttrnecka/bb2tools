9e5f8b72ea484bfe7d68cc511198a0d0f2653c60
Tomas Trnecka <trnecka@gmail.com>
2019-03-04 15:36:27 +0100

    more precise league banner and drying up of template referencing

--- GUI/automation/bb2gui.py
+++ GUI/automation/bb2gui.py
@@ -29,24 +29,24 @@ def findAndActivateWindow(name):
     return found
 
 def clickTeamManagement():
-    pos = imagesearch(os.path.join(TEMPLATE_PATH, "team_mgmt.png"))
+    pos = imagesearch(template("team_mgmt.png"))
     moveToAndClick(pos[0]+10,pos[1]+10)
-    imagesearch_loop(os.path.join(TEMPLATE_PATH, "my_leagues.png"),0.2)
+    imagesearch_loop(template("my_leagues.png"),0.2)
 
 def clickMyLeagues():
-    pos = imagesearch(os.path.join(TEMPLATE_PATH, "my_leagues.png"))
+    pos = imagesearch(template("my_leagues.png"))
     moveToAndClick(pos[0]+10,pos[1]+10)
-    imagesearch_loop(os.path.join(TEMPLATE_PATH, "right.png"),0.2)
+    imagesearch_loop(template("right.png"),0.2)
 
 def selectLeague(image_file_name):
     #move cursor to the corner so it cancels highlighting
     resetCursor()
     while True:
-        imagesearch_loop(os.path.join(TEMPLATE_PATH, "league_banner.png"),0.1,0.95)
-        pos = imagesearch(os.path.join(TEMPLATE_PATH, image_file_name),0.99)
+        imagesearch_loop(template("league_banner.png"),0.1,0.95)
+        pos = imagesearch(template(image_file_name),0.99)
         if pos[0]!=-1:
             break
-        left_arrow = imagesearch(os.path.join(TEMPLATE_PATH, "right.png"))
+        left_arrow = imagesearch(template("right.png"))
         moveToAndClick(left_arrow[0]+10,left_arrow[1]+10)
     moveToAndClick(pos[0]+10,pos[1]+10)
 
@@ -58,50 +58,50 @@ def nextCompetition():
     #move cursor to the corner so it does not mess the center pics being taken
     resetCursor()
     # wait till league settings button is visible
-    imagesearch_loop(os.path.join(TEMPLATE_PATH, "league_settings.png"),0.2,0.95)
+    imagesearch_loop(template("league_settings.png"),0.2,0.95)
     # pick start image, it will be used to tell that we went full circle
     starting_image = region_grabber(region=(860,440,1050,480))
-    starting_image.save(os.path.join(TEMPLATE_PATH, "tmp_league.png"))
+    starting_image.save(template("tmp_league.png"))
 
     while True:
-        imagesearch_loop(os.path.join(TEMPLATE_PATH, "comp_round.png"),0.1,0.95)
+        imagesearch_loop(template("comp_round.png"),0.1,0.95)
         image = pyautogui.screenshot()
         # ignore first competition as it is the starting one
         if i>1:
-            comp = imagesearcharea(os.path.join(TEMPLATE_PATH, "tmp_league.png"), 0,0,0,0,0.999, image)
+            comp = imagesearcharea(template("tmp_league.png"), 0,0,0,0,0.999, image)
             if comp[0]!=-1:
                 break
         
         yield image
         i+=1
-        right_arrow = imagesearch(os.path.join(TEMPLATE_PATH, "right.png"))
+        right_arrow = imagesearch(template("right.png"))
         moveToAndClick(right_arrow[0]+10,right_arrow[1]+10)
 
 def isCompWaitingForStart(comp_image):
-    comp = imagesearcharea(os.path.join(TEMPLATE_PATH, "waiting_for_start.png"), 0,0,0,0,0.95, comp_image)
+    comp = imagesearcharea(template("waiting_for_start.png"), 0,0,0,0,0.95, comp_image)
     if comp[0]==-1:
         return False, comp
     else:
         return True, comp
 
 def clickBack():
-    back = imagesearch_loop(os.path.join(TEMPLATE_PATH, "back.PNG"),0.1, 0.99)
+    back = imagesearch_loop(template("back.PNG"),0.1, 0.99)
     pyautogui.moveTo(back[0]+10,back[1]+10)
-    back = imagesearch_loop(os.path.join(TEMPLATE_PATH, "back_active.PNG"),0.1, 0.99)
+    back = imagesearch_loop(template("back_active.PNG"),0.1, 0.99)
     moveToAndClick(back[0]+10,back[1]+10)
 
 def waitUntilBackIsActive():
-    imagesearch_loop(os.path.join(TEMPLATE_PATH, "start_competition_active.PNG"),0.1, 0.99)
+    imagesearch_loop(template("start_competition_active.PNG"),0.1, 0.99)
 
 def startComp():
-    schedule = imagesearch_loop(os.path.join(TEMPLATE_PATH, "schedule_button.png"),0.1, 0.99)
+    schedule = imagesearch_loop(template("schedule_button.png"),0.1, 0.99)
     pyautogui.moveTo(schedule[0]+10,schedule[1]+10)
-    schedule = imagesearch_loop(os.path.join(TEMPLATE_PATH, "schedule_button_active.png"),0.1, 0.99)
+    schedule = imagesearch_loop(template("schedule_button_active.png"),0.1, 0.99)
     moveToAndClick(schedule[0]+10,schedule[1]+10)
 
-    start = imagesearch_loop(os.path.join(TEMPLATE_PATH, "start_competition.PNG"),0.1, 0.99)
+    start = imagesearch_loop(template("start_competition.PNG"),0.1, 0.99)
     pyautogui.moveTo(start[0]+10,start[1]+10)
-    start = imagesearch_loop(os.path.join(TEMPLATE_PATH, "start_competition_active.PNG"),0.1, 0.99)
+    start = imagesearch_loop(template("start_competition_active.PNG"),0.1, 0.99)
     moveToAndClick(start[0]+10,start[1]+10)
     waitUntilBackIsActive()
 
@@ -121,3 +121,6 @@ def click():
 
 def resetCursor():
     pyautogui.moveTo(1,1)
+
+def template(img_file):
+    return os.path.join(TEMPLATE_PATH, img_file)
